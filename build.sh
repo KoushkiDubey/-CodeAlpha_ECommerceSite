baa65b5c86b707f9102450f4b695617a0ea889ef
KoushkiDubey <koushkidubey123@gmail.com>
2025-06-16 20:11:45 +0530

    Initial project submission

--- build.sh
+++ build.sh
@@ -8,6 +8,10 @@ pip install gunicorn==20.1.0
 pip install psycopg2-binary==2.9.9
 pip install whitenoise==6.6.0
 pip install dj-database-url==2.1.0
+pip install Pillow==9.5.0  # ← CORRECTED VERSION
+
+# Create static directory
+mkdir -p staticfiles
 
 # Collect static files
 python manage.py collectstatic --noinput
