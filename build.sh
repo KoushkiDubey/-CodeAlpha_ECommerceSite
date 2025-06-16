f1c5e125844745dd2e386d7533240074e2fb692a
KoushkiDubey <koushkidubey123@gmail.com>
2025-06-16 14:17:23 +0530

    Initial project submission

--- build.sh
+++ build.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 set -e
 
-# Clean install without dependency conflicts
+# Install dependencies
 pip install --upgrade pip
-pip install --no-cache-dir -r requirements.txt
+pip install -r requirements.txt
 
-# Django setup
-python manage.py collectstatic --noinput
+# Apply database migrations
 python manage.py migrate
+
+# Collect static files (will now work with STATIC_ROOT set)
+python manage.py collectstatic --noinput
