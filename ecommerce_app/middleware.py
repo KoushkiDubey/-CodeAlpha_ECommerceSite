5c797989e453357ab35f90a0688471c90abd3134
KoushkiDubey <koushkidubey123@gmail.com>
2025-06-19 00:20:29 +0530

    Update middleware.py

--- ecommerce_app/middleware.py
+++ ecommerce_app/middleware.py
@@ -1,4 +1,4 @@
-# ecommerce_app/middleware.py
+
 from django.contrib import messages
 from .models import Cart
 
@@ -7,15 +7,14 @@ class ActiveCartMiddleware:
         self.get_response = get_response
 
     def __call__(self, request):
-        # Only process authenticated users
+       
         if request.user.is_authenticated:
-            # Check if user has an active cart
+           
             active_cart = Cart.objects.filter(
                 user=request.user,
                 is_active=True
             ).first()
 
-            # If no active cart exists, create one
             if not active_cart:
                 Cart.objects.create(user=request.user)
                 messages.debug(request, "New active cart created")
